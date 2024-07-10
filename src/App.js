34463b4a34990764121fb39b5b765f0bf69147cb
SuryaNesanuru <156491301+SuryaNesanuru3@users.noreply.github.com>
2024-07-10 17:42:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import NavBar from './components/NavBar';
+import theme from './theme';
+import { auth } from './firebase';
+import './styles.css';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <NavBar user={user} />
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
